40c9ffcf56df8aa5b71af95b8a920a79520daa55
Shefali Chaugule <83270502+Shefali9867@users.noreply.github.com>
2024-01-14 14:33:14 +0000

    Update app.py

--- digital_cv/app.py
+++ digital_cv/app.py
@@ -64,12 +64,6 @@ cols = st.columns(len(SOCIAL_MEDIA))
 for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
   cols[index].write(f"[{platform}]({link})")
 
-with open(main.css) as f:
-    st.markdown(f"<style>{background_effect_css}</style>", unsafe_allow_html=True)
-for platform, link in SOCIAL_MEDIA.items():
-    st.markdown(f"<a href='{link}' target='_blank'>{platform}</a>", unsafe_allow_html=True)
-
-
 
 
 # --- EXPERIENCE & QUALIFICATIONS ---
