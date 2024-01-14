6d4cdc83c86483bb4d472b308f83507423b85e73
Shefali Chaugule <83270502+Shefali9867@users.noreply.github.com>
2024-01-14 17:13:40 +0000

    Update app.py

--- digital_cv/app.py
+++ digital_cv/app.py
@@ -7,7 +7,7 @@ from PIL import Image
 #--- PATH SETTINGS---
 current_dir = Path(__file__).parent if "__file__" in locals()else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
-resume_file = current_dir / "assets" / "CV_Shefali.pdf"
+resume_file = current_dir / "assets" / "SHEFALI.pdf"
 profile_pic= current_dir / "assets" / "shef-profile-pic.png"
 linkedin_url = "https://www.linkedin.com/in/shefali-chaugule-942789200/"
 
